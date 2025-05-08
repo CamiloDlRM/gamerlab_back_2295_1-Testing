96c1e4f535236167d32f395fbcaafe1826ec7722
CamiloDlRM <zinbak78@gmail.com>
2025-05-08 20:25:30 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from "express";
 
+// Crear servidor Express fuera de la función bootstrap
+const server = express();
+
 async function bootstrap() {
-  const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors(); // Añadir CORS para APIs
   await app.init();
-  
 }
+
+// Iniciar la aplicación
 bootstrap();
+
+// Exportar el servidor Express para Vercel
+export default server;
