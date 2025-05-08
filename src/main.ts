0c0ecfcdb633c3bb97ee06e2b9033683742b639b
CamiloDlRM <zinbak78@gmail.com>
2025-05-08 19:37:49 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('Servidor corriendo en http://localhost:3000');
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.useGlobalPipes(new ValidationPipe());
+  await app.init();
+  
+  // Este `listen` no es necesario para funciones serverless, pero puedes dejarlo para propósitos locales
+  const port = process.env.PORT ?? 3000;
+  server.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+  });
 }
 bootstrap();
